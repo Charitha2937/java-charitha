740c99daa333f967b9ff7455f6e58d94def74b2c
Ubuntu <ubuntu@ip-172-31-45-116.eu-north-1.compute.internal>
2026-01-31 09:06:52 +0000

    no leaks found

--- src/main/java/hello/SecretController.java
+++ src/main/java/hello/SecretController.java
@@ -1,5 +1,8 @@
 @RestController
 public class SecretController {
-    // Hardcoded API Token
-    private String githubToken = "ghp_nS1yOEcfP5pvfqJml36mF7AkyHsEU0IU36mF";
+
+    // Read token securely from environment variable
+    private String githubToken = System.getenv("GITHUB_TOKEN");
+
 }
+
